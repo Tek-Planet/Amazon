33d2c7dfbceab34a8f2b537547eaa29c9ee95b5d
tekplanet <tekplanet@tekplanet.tekplanet>
2020-11-11 16:50:42 +0100

    Basket Counter and cost summation

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useHistory } from "react-router-dom";
-
+import { getBasketTotal } from "./reducer";
+import { useStateValue } from "./StateProvider";
 
 
 function Subtotal() {
   const history = useHistory();
-  
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -15,7 +17,7 @@ function Subtotal() {
           <>
             <p>
               {/* Part of the homework */}
-              Subtotal (0 items): <strong>{value}</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -23,7 +25,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)} // Part of the homework
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
